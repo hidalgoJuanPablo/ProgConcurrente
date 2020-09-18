949d524bab9cca32b76444d39404c39163d1da17
FranciscoManuelRepetto <70414317+FranciscoManuelRepetto@users.noreply.github.com>
2020-09-18 19:17:11 -0300

    Saque sleep de metodo sincronizado

--- Tp3Synchronized/Punto4/Estacion.java
+++ Tp3Synchronized/Punto4/Estacion.java
@@ -7,10 +7,6 @@ public class Estacion {
     public synchronized void cargarNafta(Auto unAuto){
         unAuto.setKmRecorridos(0);
         System.out.println("ESTA CARGANDO NAFTA: "+unAuto.getPatente());
-        try{
-                Thread.sleep(2000);
-            }catch(InterruptedException ex){
-                System.out.println("Hubo error");
-        }
+      
     }    
 }
