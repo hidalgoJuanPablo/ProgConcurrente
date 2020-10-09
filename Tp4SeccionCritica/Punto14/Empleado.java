49b4e092ad67923e0acc5b4b01feb28775f9cfe7
FranciscoManuelRepetto <70414317+FranciscoManuelRepetto@users.noreply.github.com>
2020-10-09 09:55:48 -0300

    Agregue metodos bebiendo y comiendo

--- Tp4SeccionCritica/Punto14/Empleado.java
+++ Tp4SeccionCritica/Punto14/Empleado.java
@@ -18,13 +18,17 @@ public class Empleado implements Runnable {
                 switch (accion) {
                     case 1:                        
                         SolicitarBebida();
+                        bebiendo();
                         break;
                     case 2:
                         SolicitarComida();
+                        comiendo();
                         break;
                     case 3:
                         SolicitarBebida();
+                        bebiendo();
                         SolicitarComida();
+                        comiendo();
                         break;
                 }
                 confiteria.LiberarLugar();
@@ -36,8 +40,30 @@ public class Empleado implements Runnable {
     public void SolicitarBebida() {
         confiteria.SolicitarMozo();
     }
+    
+    private void bebiendo(){
+        /*Metodo que simula al empleado bebiendo*/
+        try {
+            System.out.println(Thread.currentThread().getName() + " esta bebiendo");
+            Thread.sleep(5000);
+            System.out.println(Thread.currentThread().getName() + " termino de beber");
+        } catch (InterruptedException ex) {
+             System.out.println("Hubo un error");
+        }
+    }
 
     public void SolicitarComida() {
         confiteria.SolicitarCocinero();
+    }
+    
+    private void comiendo(){
+       /*Metodo que simula al empleado comiendo*/
+       try {
+           System.out.println(Thread.currentThread().getName() + " esta comiendo");
+           Thread.sleep(5000);
+           System.out.println(Thread.currentThread().getName() + " termino de comer");
+       } catch (InterruptedException ex) {
+            System.out.println("Hubo un error");
+       }
     }
 }
